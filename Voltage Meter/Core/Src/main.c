9b66caa923b8063e7577ca8f273050b9ad8b5050
napassorn29 <119843578+napassorn29@users.noreply.github.com>
2023-02-11 21:52:56 +0700

    setting

--- Voltage Meter/Core/Src/main.c
+++ Voltage Meter/Core/Src/main.c
@@ -196,7 +196,7 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_56CYCLES;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
